1c6f1cd3107518819637be2af291211305d7b6b7
Chad-Mowbray <mowbray.chad@gmail.com>
2020-02-20 21:05:29 -0600

    add posts table to db

--- database/initialize_database.py
+++ database/initialize_database.py
@@ -11,9 +11,13 @@ cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND 
 if cursor.fetchone()[0] == 1:
     print(f'{db_name} already exists...')
 else:
-    print(f'{db_name}does not exist yet, creating it now...')
+    print(f'{db_name} does not exist yet, creating it now...')
     cursor.execute("CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT UNIQUE, email TEXT, password TEXT)")
     db.commit()
+    cursor.execute("CREATE TABLE posts(id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, body TEXT, FOREIGN KEY (user_id) REFERENCES users (id))")
+    db.commit()
+
+    # , FOREIGN KEY (user_id) REFERENCES users (id)
 
     req = requests.get("https://jsonplaceholder.typicode.com/users")
 
@@ -37,6 +41,29 @@ else:
 
         print('added a new user')
     db.commit()
+
+
+    req2 = requests.get("https://jsonplaceholder.typicode.com/posts")
+    posts = req2.json()
+
+    for post in posts:
+        user_id = 0
+        title = ''
+        body = ''
+        for k,v in post.items():
+            if k == "userId":
+                user_id = v
+            if k == "title":
+                title = v
+            if k == "body":
+                body = v
+        
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO posts(user_id, title, body)\nVALUES(?,?,?)", (user_id, title, body))
+
+        print('added a new post')
+    db.commit()
+
     db.close()
     print("*********database is populated************")
 
